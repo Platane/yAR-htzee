a6b63b02879031a1c714cd614b9cbd6e65497ae7
platane <me@platane.me>
2021-01-18 12:57:07 +0100

    🚀add dice physic

--- src/App/Dice.tsx
+++ src/App/Dice.tsx
@@ -6,47 +6,15 @@ const diceUrl = "/dice.glb";
 useGLTF.preload(diceUrl);
 
 export const Dice = (props: any) => {
-  const gltf = useGLTF(diceUrl);
+  const { nodes, materials } = useGLTF(diceUrl);
 
-  React.useEffect(() => {
-    gltf.scene.traverse((o) => {
-      o.castShadow = true;
-      // o.receiveShadow = true;
-    });
-  }, [gltf.scene]);
-
-  return <primitive scale={[0.2, 0.2, 0.2]} {...props} object={gltf.scene} />;
+  return (
+    <mesh
+      castShadow
+      {...props}
+      material={materials.Dice}
+      geometry={(nodes.mesh_0 as any).geometry}
+      dispose={null}
+    />
+  );
 };
-
-// import { useLoader } from "react-three-fiber";
-// import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
-// import { GLTFExporter } from "three/examples/jsm/exporters/GLTFExporter";
-// import * as THREE from "three";
-// const loader = new GLTFLoader();
-// loader
-//   .loadAsync(diceUrl)
-//   .then(async ({ scene }) => {
-//     const e = new GLTFExporter();
-//     const binary = await new Promise<any>((resolve) =>
-//       e.parse(scene, resolve, { binary: true })
-//     );
-
-//     // Create an invisible A element
-//     const a = document.createElement("a");
-//     a.style.display = "none";
-//     document.body.appendChild(a);
-
-//     // Set the HREF to a Blob representation of the data to be downloaded
-//     a.href = window.URL.createObjectURL(new Blob([binary]));
-
-//     // Use download attribute to set set desired file name
-//     a.setAttribute("download", "a.glb");
-
-//     // Trigger the download by simulating click
-//     a.click();
-
-//     // Cleanup
-//     window.URL.revokeObjectURL(a.href);
-//     document.body.removeChild(a);
-//   })
-//   .catch(console.error);
--- src/App/Game.tsx
+++ src/App/Game.tsx
@@ -1,12 +1,102 @@
 import * as React from "react";
 import { Dice } from "./Dice";
+import { Api, Physics, useBox, usePlane } from "@react-three/cannon";
+import { useFrame } from "react-three-fiber";
+import { getDiceUpFace } from "../utils/getDiceFaceUp";
 
 type Props = {};
 
 export const Game = ({}: Props) => {
   return (
     <>
-      <Dice position={[0, 0.2, 0]} />
+      <Physics>
+        <Plane />
+        <Dices />
+      </Physics>
     </>
+  );
+};
+
+const useSystemStability = (boxes: Api[], onStable: () => void) => {
+  const sleep = React.useRef({ stableVelocities: [false], k: 0 });
+
+  useFrame(() => {
+    if (sleep.current.stableVelocities.every(Boolean)) sleep.current.k++;
+    else sleep.current.k = 0;
+
+    if (sleep.current.k === 10) onStable();
+  });
+
+  React.useEffect(() => {
+    const unsubscribes: any[] = boxes
+      .map(([, api], i) => {
+        return [
+          api.velocity.subscribe((v) => {
+            sleep.current.stableVelocities[i * 2 + 0] = Math.hypot(...v) < 0.04;
+          }),
+          api.angularVelocity.subscribe((v) => {
+            sleep.current.stableVelocities[i * 2 + 1] = Math.hypot(...v) < 0.12;
+          }),
+        ];
+      })
+      .flat();
+
+    const unsubscribeAll = () => unsubscribes.forEach((u) => u());
+
+    return unsubscribeAll;
+  }, []);
+};
+
+const Dices = () => {
+  const boxes = Array.from({ length: 6 }, () =>
+    useBox(() => ({
+      mass: 1,
+      args: [0.2, 0.2, 0.2],
+    }))
+  );
+
+  const roll = () => {
+    boxes.forEach(([_, api]) => {
+      api.position.set(
+        (Math.random() - 0.5) * 0.5,
+        1 + Math.random() * 1,
+        (Math.random() - 0.5) * 0.5 + 2
+      );
+      api.rotation.set(
+        Math.random() * Math.PI,
+        Math.random() * Math.PI,
+        Math.random() * Math.PI
+      );
+      api.velocity.set(0, 0, -3.5);
+      api.angularVelocity.set(Math.random(), 0, 0);
+    });
+  };
+
+  React.useEffect(roll, []);
+
+  useSystemStability(boxes, () => {
+    console.log(boxes.map(([ref]) => getDiceUpFace(ref.current)));
+    roll();
+  });
+
+  return (
+    <>
+      {boxes.map(([ref], i) => (
+        <group ref={ref} key={i}>
+          <Dice scale={[0.1, 0.1, 0.1]}></Dice>
+        </group>
+      ))}
+    </>
+  );
+};
+
+const Plane = () => {
+  const [ref] = usePlane(() => ({ rotation: [-Math.PI / 2, 0, 0] }));
+
+  return (
+    <mesh ref={ref} visible={false}>
+      <planeBufferGeometry attach="geometry" args={[1009, 1000]} />
+      <meshStandardMaterial attach="material" color="#171717" />
+    </mesh>
   );
 };
