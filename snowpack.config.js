ecd1b66de414e40d254146048006d0022b601859
platane <me@platane.me>
2021-05-06 09:51:31 +0200

    🚀test rollup build

--- snowpack.config.js
+++ snowpack.config.js
@@ -1,11 +1,18 @@
 require("dotenv").config();
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
+const { visualizer } = require("rollup-plugin-visualizer");
 
 module.exports = {
   mount: {
     public: "/",
     src: "/_dist_",
   },
+
+  // alias: {
+  //   "three/examples/js/libs/stats.min": "three/examples/js/libs/stats.min",
+  //   three: __dirname + "/node_modules/three/src/Three.js",
+  // },
+
   plugins: [
     //
     "@snowpack/plugin-typescript",
@@ -25,6 +32,20 @@ module.exports = {
         },
       },
     ],
+
+    // [
+    //   "snowpack-plugin-rollup-bundle",
+    //   {
+    //     entrypoints: "build/_dist_/index.js",
+    //     emitHtmlFiles: true,
+    //     extendConfig: (config) => {
+    //       config.inputOptions.plugins.push(
+    //         visualizer({ template: "treemap", filename: "build/report.html" })
+    //       );
+    //       return config;
+    //     },
+    //   },
+    // ],
 
     [
       "snowpack-plugin-replace",
